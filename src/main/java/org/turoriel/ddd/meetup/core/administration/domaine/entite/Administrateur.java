2db81ed8908122839e39e7a3c139a63c9e0a9416
Diarra <diarramamadou73@gmail.com>
2020-06-15 16:06:19 +0000

    Admistrateuré

--- src/main/java/org/turoriel/ddd/meetup/core/administration/domaine/entite/Administrateur.java
+++ src/main/java/org/turoriel/ddd/meetup/core/administration/domaine/entite/Administrateur.java
@@ -8,11 +8,11 @@ import org.turoriel.ddd.meetup.core.noyaupartage.domaine.entite.Utilisateur;
  * @author Diarra Mamadou 2020-06-15
  */
 public class Administrateur extends Utilisateur {
-    /*
+
     public Administrateur() {}
 
     public Administrateur(String nom, String prenom, int age) {
         super(nom, prenom, age);
     }
-    */
+    
 }
